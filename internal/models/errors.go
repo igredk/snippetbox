216de57b5060fc6142c4ba0b0366a25050a5eee9
Редькин Игорь <igredk@gmail.com>
2024-08-20 21:40:53 +0200

    Implement userSignupPost handler.

--- internal/models/errors.go
+++ internal/models/errors.go
@@ -4,6 +4,10 @@ import (
 	"errors"
 )
 
+const (
+	UniqueConstraintViolation = "23505"
+)
+
 var (
 	ErrNoRecord           = errors.New("models: no matching record found")
 	ErrInvalidCredentials = errors.New("models: invalid credentials")
--- internal/models/users.go
+++ internal/models/users.go
@@ -1,9 +1,13 @@
 package models
 
 import (
+	"context"
+	"errors"
 	"time"
 
+	"github.com/jackc/pgx/v5/pgconn"
 	"github.com/jackc/pgx/v5/pgxpool"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type User struct {
@@ -19,6 +23,23 @@ type UserModel struct {
 }
 
 func (m *UserModel) Insert(name, email, password string) error {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 12)
+	if err != nil {
+		return err
+	}
+	query := `INSERT INTO users (name, email, hashed_password, created)
+    VALUES($1, $2, $3, NOW()) RETURNING id`
+
+	var id int
+	err = m.DB.QueryRow(context.Background(), query, name, email, string(hashedPassword)).Scan(&id)
+	if err != nil {
+		var ucError *pgconn.PgError
+		if errors.As(err, &ucError) {
+			if ucError.Code == UniqueConstraintViolation {
+				return ErrDuplicateEmail
+			}
+		}
+	}
 	return nil
 }
 
